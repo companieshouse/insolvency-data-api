0d760f690d594b9d90a295f1df49d2b7cff481bb
Vamshidhar Chitti <vchitti@companieshouse.gov.uk@pd12638.companieshouse.local>
2022-02-10 12:22:48 +0000

    DSND-424: Fixed review comments

--- src/main/java/uk/gov/companieshouse/insolvency/data/InsolvencyDataApiApplication.java
+++ src/main/java/uk/gov/companieshouse/insolvency/data/InsolvencyDataApiApplication.java
@@ -2,8 +2,10 @@ package uk.gov.companieshouse.insolvency.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoDataAutoConfiguration.class, MongoAutoConfiguration.class})
 public class InsolvencyDataApiApplication {
 
     public static void main(String[] args) {
