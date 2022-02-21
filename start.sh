ea2bf1412dc3da5dd3e0546bfb6bb509ee8c199f
Ram Krishnamurthy <rkrishnamurthy@companieshouse.gov.uk>
2022-02-21 10:29:53 +0000

    changes to start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ -z "${MESOS_SLAVE_PID}" ]]; then
     source ~/.chs_env/private_env
     source ~/.chs_env/global_env
-    source ~/.chs_env/insolvency-delta-consumer/env
+    source ~/.chs_env/insolvency-data-api/env
 
     PORT="${OFFICER_DELTA_PROCESSOR_PORT}"
 else
