b85d0c7ac6334e49a29391f1ba1881ada93fc52f
zahidm3 <zmuhammad@companieshouse.gov.uk>
2022-03-22 11:57:05 +0000

    Read and Write converters for enums

--- src/main/java/uk/gov/companieshouse/insolvency/data/config/ApplicationConfig.java
+++ src/main/java/uk/gov/companieshouse/insolvency/data/config/ApplicationConfig.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -19,6 +20,7 @@ import uk.gov.companieshouse.environment.EnvironmentReader;
 import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
 import uk.gov.companieshouse.insolvency.data.converter.CompanyInsolvencyReadConverter;
 import uk.gov.companieshouse.insolvency.data.converter.CompanyInsolvencyWriteConverter;
+import uk.gov.companieshouse.insolvency.data.converter.EnumConverters;
 import uk.gov.companieshouse.insolvency.data.serialization.LocalDateDeSerializer;
 import uk.gov.companieshouse.insolvency.data.serialization.LocalDateSerializer;
 import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
@@ -41,16 +43,21 @@ public class ApplicationConfig implements WebMvcConfigurer {
     public MongoCustomConversions mongoCustomConversions() {
         ObjectMapper objectMapper = mongoDbObjectMapper();
         return new MongoCustomConversions(List.of(new CompanyInsolvencyWriteConverter(objectMapper),
-                new CompanyInsolvencyReadConverter(objectMapper)));
+                new CompanyInsolvencyReadConverter(objectMapper),
+                new EnumConverters.StringToEnum(),
+                new EnumConverters.EnumToString()));
     }
 
     private ObjectMapper mongoDbObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         SimpleModule module = new SimpleModule();
+
         module.addSerializer(LocalDate.class, new LocalDateSerializer());
         module.addDeserializer(LocalDate.class, new LocalDateDeSerializer());
+        // TODO: Add Serializer and De-Serializer for OffsetDateTime as well like above
         objectMapper.registerModule(module);
 
         return objectMapper;
