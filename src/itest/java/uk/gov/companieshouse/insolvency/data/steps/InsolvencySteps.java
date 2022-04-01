5bb7b77f62f08028459d38052bd892c652875dee
Vamshidhar Chitti <vchitti@pd12634.companieshouse.local>
2022-04-01 17:53:03 +0100

    DSND-691: Added feature file for get request

--- src/itest/java/uk/gov/companieshouse/insolvency/data/steps/InsolvencySteps.java
+++ src/itest/java/uk/gov/companieshouse/insolvency/data/steps/InsolvencySteps.java
@@ -4,26 +4,30 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.assertj.core.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.*;
 import uk.gov.companieshouse.api.insolvency.CompanyInsolvency;
 import uk.gov.companieshouse.api.insolvency.InternalCompanyInsolvency;
+import uk.gov.companieshouse.api.insolvency.InternalData;
+import uk.gov.companieshouse.insolvency.data.config.CucumberContext;
 import uk.gov.companieshouse.insolvency.data.model.InsolvencyDocument;
+import uk.gov.companieshouse.insolvency.data.model.Updated;
 import uk.gov.companieshouse.insolvency.data.repository.InsolvencyRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 public class InsolvencySteps {
 
     private String companyNumber;
-
-    private ResponseEntity<Void> response;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -34,12 +38,39 @@ public class InsolvencySteps {
     @Autowired
     private InsolvencyRepository insolvencyRepository;
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     @Before
     public void dbCleanUp(){
         insolvencyRepository.deleteAll();
     }
 
-    @When("I send PUT request with payload {string}")
+    @Given("the insolvency information exists for {string}")
+    public void the_insolvency_information_exists_for(String companyNumber) throws IOException {
+        File file = new ClassPathResource("/json/input/case_type_compulsory_liquidation.json").getFile();
+        InternalCompanyInsolvency companyInsolvency = objectMapper.readValue(file, InternalCompanyInsolvency.class);
+
+        InternalData internalData = companyInsolvency.getInternalData();
+        Updated updated = new Updated(internalData.getDeltaAt().toString(),
+                internalData.getUpdatedBy(), "company-insolvency");
+        InsolvencyDocument insolvencyDocument = new InsolvencyDocument(companyNumber,
+                companyInsolvency.getExternalData(), updated);
+
+        mongoTemplate.save(insolvencyDocument);
+    }
+
+    @When("I send GET request with company number {string}")
+    public void i_send_get_request_with_company_number(String companyNumber) throws IOException {
+        String uri = "/company/{company_number}/insolvency";
+        ResponseEntity<CompanyInsolvency> response = restTemplate.exchange(uri, HttpMethod.GET, null,
+                CompanyInsolvency.class, companyNumber);
+
+        CucumberContext.CONTEXT.set("statusCode", response.getStatusCodeValue());
+        CucumberContext.CONTEXT.set("getResponseBody", response.getBody());
+    }
+
+    @When("I send PUT request with payload {string} file")
     public void i_send_put_request_with_payload(String string) throws IOException {
         File file = new ClassPathResource("/json/input/" + string + ".json").getFile();
         InternalCompanyInsolvency companyInsolvency = objectMapper.readValue(file, InternalCompanyInsolvency.class);
@@ -55,14 +86,16 @@ public class InsolvencySteps {
         ResponseEntity<Void> response = restTemplate.exchange(uri, HttpMethod.PUT, request, Void.class, companyNumber);
 
         this.companyNumber = companyNumber;
-        this.response = response;
-    }
-    @Then("I should receive {int} status code")
-    public void i_should_receive_status_code(Integer statusCode) {
-        Assertions.assertThat(this.response.getStatusCode()).isSameAs(HttpStatus.valueOf(statusCode));
+        CucumberContext.CONTEXT.set("statusCode", response.getStatusCodeValue());
     }
 
-    @Then("the Get request should match {string}")
+    @Then("I should receive {int} status code")
+    public void i_should_receive_status_code(Integer statusCode) {
+        int expectedStatusCode = CucumberContext.CONTEXT.get("statusCode");
+        Assertions.assertThat(expectedStatusCode).isEqualTo(statusCode);
+    }
+
+    @Then("the expected result should match {string} file")
     public void the_expected_result_should_match(String string) throws IOException {
         File file = new ClassPathResource("/json/output/" + string + ".json").getFile();
 
@@ -70,19 +103,36 @@ public class InsolvencySteps {
 
         Assertions.assertThat(insolvencyDocuments).hasSize(1);
 
-        String uri = "/company/{company_number}/insolvency";
-        String companyNumber = "CH5324324";
-        ResponseEntity<CompanyInsolvency> actual = restTemplate.exchange(uri, HttpMethod.GET, null,
-                CompanyInsolvency.class, companyNumber);
+        Optional<InsolvencyDocument> actual = insolvencyRepository.findByCompanyNumber(this.companyNumber);
 
+        assertThat(actual.isPresent()).isTrue();
 
         InsolvencyDocument expected = objectMapper.readValue(file, InsolvencyDocument.class);
 
-        verifyData(actual.getBody(), expected.getCompanyInsolvency());
+        verifyPutData(actual.get(), expected);
     }
 
-    private void verifyData(CompanyInsolvency actual, CompanyInsolvency expected) {
-        assertThat(actual.getCases()).isEqualTo(expected.getCases());
-        assertThat(actual.getStatus()).isEqualTo(expected.getStatus());
+    @Then("the Get call response body should match {string} file")
+    public void the_get_call_response_body_should_match(String dataFile) throws IOException {
+        File file = new ClassPathResource("/json/output/"+dataFile+".json").getFile();
+
+        InsolvencyDocument expectedDocument = objectMapper.readValue(file, InsolvencyDocument.class);
+
+        CompanyInsolvency expected = expectedDocument.getCompanyInsolvency();
+        CompanyInsolvency actual = CucumberContext.CONTEXT.get("getResponseBody");
+
+        assertThat(expected.getStatus()).isEqualTo(actual.getStatus());
+        assertThat(expected.getCases()).isEqualTo(actual.getCases());
     }
+
+    private void verifyPutData(InsolvencyDocument actual, InsolvencyDocument expected) {
+        CompanyInsolvency actualCompanyInsolvency = actual.getCompanyInsolvency();
+        CompanyInsolvency expectedCompanyInsolvency = expected.getCompanyInsolvency();
+
+        assertThat(actualCompanyInsolvency.getCases()).isEqualTo(expectedCompanyInsolvency.getCases());
+        assertThat(actual.getCompanyNumber()).isEqualTo(expected.getCompanyNumber());
+        assertThat(actual.getUpdated().getType()).isEqualTo(expected.getUpdated().getType());
+        assertThat(actual.getUpdated().getBy()).isEqualTo(expected.getUpdated().getBy());
+    }
+
 }
--- src/itest/resources/features/company-insolvency.feature
+++ src/itest/resources/features/company-insolvency.feature
@@ -3,11 +3,23 @@ Feature: Process company insolvency information
   Scenario Outline: Processing company insolvency information successfully
 
     Given Insolvency data api service is running
-    When I send PUT request with payload "<data>"
+    When I send PUT request with payload "<data>" file
     Then I should receive 200 status code
-    And the Get request should match "<result>"
+    And the expected result should match "<result>" file
 
     Examples:
       | data                             | result                                  |
       | case_type_compulsory_liquidation | case_type_compulsory_liquidation_output |
       | case_type_receivership           | case_type_receivership_output           |
+
+  Scenario Outline: Retrieve company insolvency information successfully
+
+    Given Insolvency data api service is running
+    And the insolvency information exists for "<companyNumber>"
+    When I send GET request with company number "<companyNumber>"
+    Then I should receive 200 status code
+    And the Get call response body should match "<result>" file
+
+    Examples:
+      | companyNumber | result                     |
+      | CH3634545     | retrieve_by_company_number |
