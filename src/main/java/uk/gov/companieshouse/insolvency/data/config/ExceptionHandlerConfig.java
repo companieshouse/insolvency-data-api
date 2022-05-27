56ef3f890d1a0f1486932fbbe7a0d223c7657fab
gianmarios-ch <gseregni@companieshouse.gov.uk>
2022-05-27 14:18:36 +0100

    Adding JSON Payload Validation fot Request Body in PUT method

--- src/main/java/uk/gov/companieshouse/insolvency/data/config/ExceptionHandlerConfig.java
+++ src/main/java/uk/gov/companieshouse/insolvency/data/config/ExceptionHandlerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -76,7 +77,7 @@ public class ExceptionHandlerConfig {
      * @return error response to return.
      */
     @ExceptionHandler(value = {BadRequestException.class, DateTimeParseException.class,
-            HttpMessageNotReadableException.class})
+            HttpMessageNotReadableException.class, MethodArgumentNotValidException.class})
     public ResponseEntity<Object> handleBadRequestException(Exception ex,
                                                             WebRequest request) {
         String errMsg = "Bad request";
